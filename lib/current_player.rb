10737f0f44ed663791b309c9757cf2177b2582b3
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-03 23:58:09 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -13,5 +13,5 @@ def current_player(board)
   return turn_count(board)%2 == 1 ? "X" : "O"
 end
 
-board = Array.new(9, " ")
+board = board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 puts turn_count(board)
